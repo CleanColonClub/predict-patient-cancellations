ab3a8772d7d906d548a493f227e5787e3ec9192e
jkozol <jacobdkozol@gmail.com>
2018-11-14 23:00:23 -0500

    temp changes

--- logitclass.py
+++ logitclass.py
@@ -18,21 +18,6 @@ def loadData():
     y = dataset[target]
     return(X, y)
 
-def train(X, y):
-    old_stdout = sys.stdout
-    # sys.stdout = mystdout = StringIO()
-    clf = SGDClassifier(verbose=1)
-    clf.fit(X, y)
-    sys.stdout = old_stdout
-    loss_history = mystdout.getvalue()
-    loss_list = []
-    for line in loss_history.split('\n'):
-        if(len(line.split("loss: ")) == 1):
-            continue
-        loss_list.append(float(line.split("loss: ")[-1]))
-    pass
-
-
 def test():
     dataset = pd.read_csv('data/data.csv')
     feature_cols = ['Date Diff']
